a184769c130a1350a962329c212d6f6103900d4a
Nemi1230 <nemiakuma@gmail.com>
2022-09-03 15:14:47 +0900

    Code rewrite

--- src/main/java/jp/nemi/hardcore/HCCore.java
+++ src/main/java/jp/nemi/hardcore/HCCore.java
@@ -1,30 +1,40 @@
 package jp.nemi.hardcore;
 
-import jp.nemi.hardcore.util.HCLog;
-import net.minecraft.block.Blocks;
+import jp.nemi.hardcore.init.HCBlocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(HCCore.MOD_ID)
 public class HCCore {
+    public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "hardcore";
+    public static HCCore instance;
 
     public HCCore() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modBus.addListener(this::init);
+        modBus.addListener(this::clientSetup);
 
+        HCBlocks.BLOCKS.register(modBus);
+
+        instance = this;
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        HCLog.info("HELLO FROM PREINIT");
-        HCLog.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    private void init(final FMLCommonSetupEvent event) {
+
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        HCLog.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+    private void clientSetup(final FMLClientSetupEvent event) {
+    }
+
+    public static HCCore getInstance() {
+        return instance;
     }
 }
