60b0580ffae81c6e1bbf6002a611f557b74d71f9
Nemi1230 <nemiakuma@gmail.com>
2022-09-03 15:11:36 +0900

    Add Files

---
+++ src/main/java/jp/nemi/hardcore/object/blocks/StickBlock.java
@@ -0,0 +1,35 @@
+package jp.nemi.hardcore.object.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+
+public class StickBlock extends Block {
+    protected static final VoxelShape AABB = Block.box(6.0D, 0.0D, 6.0D, 10.0D, 10.0D, 10.0D);
+
+    public StickBlock(Block.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
+        return AABB;
+    }
+
+    @Override
+    public BlockState updateShape(BlockState p_196271_1_, Direction p_196271_2_, BlockState p_196271_3_, IWorld p_196271_4_, BlockPos p_196271_5_, BlockPos p_196271_6_) {
+        return p_196271_2_ == Direction.DOWN && !canSurvive(p_196271_1_, p_196271_4_, p_196271_5_) ? Blocks.AIR.defaultBlockState() : super.updateShape(p_196271_1_, p_196271_2_, p_196271_3_, p_196271_4_, p_196271_5_, p_196271_6_);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState p_196260_1_, IWorldReader p_196260_2_, BlockPos p_196260_3_) {
+        return canSupportCenter(p_196260_2_, p_196260_3_.below(), Direction.UP);
+    }
+}
---
+++ src/main/java/jp/nemi/hardcore/object/blocks/WallStickBlock.java
@@ -0,0 +1,97 @@
+package jp.nemi.hardcore.object.blocks;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class WallStickBlock extends StickBlock {
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
+    private static final Map<Direction, VoxelShape> AABBS = Maps.newEnumMap(ImmutableMap.of(
+            Direction.NORTH, Block.box(5.5D, 3.0D, 11.0D, 10.5D, 13.0D, 16.0D),
+            Direction.SOUTH, Block.box(5.5D, 3.0D, 0.0D, 10.5D, 13.0D, 5.0D),
+            Direction.WEST, Block.box(11.0D, 3.0D, 5.5D, 16.0D, 13.0D, 10.5D),
+            Direction.EAST, Block.box(0.0D, 3.0D, 5.5D, 5.0D, 13.0D, 10.5D)));
+
+    public WallStickBlock(AbstractBlock.Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public String getDescriptionId() {
+        return this.asItem().getDescriptionId();
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
+        return getShape(p_220053_1_);
+    }
+
+    public static VoxelShape getShape(BlockState p_220289_0_) {
+        return AABBS.get(p_220289_0_.getValue(FACING));
+    }
+
+    @Override
+    public boolean canSurvive(BlockState p_196260_1_, IWorldReader p_196260_2_, BlockPos p_196260_3_) {
+        Direction direction = p_196260_1_.getValue(FACING);
+        BlockPos blockpos = p_196260_3_.relative(direction.getOpposite());
+        BlockState blockstate = p_196260_2_.getBlockState(blockpos);
+        return blockstate.isFaceSturdy(p_196260_2_, blockpos, direction);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_) {
+        BlockState blockstate = this.defaultBlockState();
+        IWorldReader iworldreader = p_196258_1_.getLevel();
+        BlockPos blockpos = p_196258_1_.getClickedPos();
+        Direction[] adirection = p_196258_1_.getNearestLookingDirections();
+
+        for(Direction direction : adirection) {
+            if (direction.getAxis().isHorizontal()) {
+                Direction direction1 = direction.getOpposite();
+                blockstate = blockstate.setValue(FACING, direction1);
+                if (blockstate.canSurvive(iworldreader, blockpos)) {
+                    return blockstate;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public BlockState updateShape(BlockState p_196271_1_, Direction p_196271_2_, BlockState p_196271_3_, IWorld p_196271_4_, BlockPos p_196271_5_, BlockPos p_196271_6_) {
+        return p_196271_2_.getOpposite() == p_196271_1_.getValue(FACING) && !p_196271_1_.canSurvive(p_196271_4_, p_196271_5_) ? Blocks.AIR.defaultBlockState() : p_196271_1_;
+    }
+
+    @Override
+    public BlockState rotate(BlockState p_185499_1_, Rotation p_185499_2_) {
+        return p_185499_1_.setValue(FACING, p_185499_2_.rotate(p_185499_1_.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState p_185471_1_, Mirror p_185471_2_) {
+        return p_185471_1_.rotate(p_185471_2_.getRotation(p_185471_1_.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> p_206840_1_) {
+        p_206840_1_.add(FACING);
+    }
+}
